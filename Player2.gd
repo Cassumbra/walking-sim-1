fa4584bc87e552d41ea3b06a3acc4068c2e62a00
Cassumbra <penumbruh@gmail.com>
2021-01-05 02:53:10 -0600

    about to refactor
    going to clean up the player controller script before i start adding objects for it to interact with

--- Player2.gd
+++ Player2.gd
@@ -1,16 +1,17 @@
 extends KinematicBody
 
-const GRAVITY = 9.81
+const GRAVITY = 40
 
-const JUMP_POWER = 7
-const MOVE_SPEED = 5.5
-const AIR_MOVE_SPEED = 15
+const JUMP_POWER = 20
 const AIR_ACCEL = 1
 const NORMAL_ACCEL = 6
 const MAX_SLOPE_ANGLE = 80
+const CROUCH = 1.75
+const NORM_SPEED = 20
 
 var h_accel = 6
 
+var move_speed = 20
 var velocity = Vector3()
 var direction = Vector3()
 var movement = Vector3()
@@ -24,6 +25,7 @@ var can_step = false
 var camera
 onready var head = $Head
 onready var ground_check = $GroundCheck
+onready var body = $Body
 onready var label = get_node("../HUD/Crosshair/Label")
 onready var label2 = get_node("../HUD/Crosshair/Label2")
 
@@ -60,6 +62,24 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("movement_jump") and (is_on_floor() or full_contact):
 		gravity_vec = Vector3.UP * JUMP_POWER
 		snap = Vector3(0, 0, 0)
+		
+	if Input.is_action_just_pressed("movement_crouch"):
+		body.scale.y -= CROUCH
+		head.translation.y += -CROUCH - 0.1
+		move_speed = NORM_SPEED / 2
+		
+		head_check.translation.y = body.scale.y
+		body_check.translation.y = (body.scale.y / 2) -0.5
+		foot_check.translation.y = -body.scale.y
+	elif Input.is_action_just_released("movement_crouch"):
+		body.scale = Vector3(1, 3, 1)
+		head.translation = Vector3(0, 2, 0)
+		move_speed = NORM_SPEED
+	
+		head_check.translation.y = body.scale.y
+		body_check.translation.y = (body.scale.y / 2) -0.5
+		foot_check.translation.y = -body.scale.y
+		
 	
 	if Input.is_action_pressed("movement_forward"):
 		direction += transform.basis.z
@@ -71,32 +91,36 @@ func _physics_process(delta):
 		direction -= transform.basis.x
 
 	direction = direction.normalized()
-	h_velocity = h_velocity.linear_interpolate(direction * MOVE_SPEED, h_accel * delta)
+	h_velocity = h_velocity.linear_interpolate(direction * move_speed, h_accel * delta)
 	movement.z = h_velocity.z + gravity_vec.z
 	movement.x = h_velocity.x + gravity_vec.x
 	movement.y = gravity_vec.y
 	
+	#Stairs!
+	if is_on_wall():
+		if is_on_floor():
+			for i in range(get_slide_count()):
+				var collision = get_slide_collision(i)
+				if collision.normal.y != 1:
+					head_check.cast_to = Vector3(direction.x*1.5, 0, direction.z*1.5)
+					body_check.cast_to = Vector3(direction.x*1.5, 0, direction.z*1.5)
+					foot_check.cast_to = Vector3(direction.x*1.5, 0, direction.z*1.5)
+					if (not head_check.is_colliding()) and (not body_check.is_colliding()) and (foot_check.is_colliding()):
+						move_and_collide(Vector3(0, JUMP_POWER, 0))
+						move_and_collide(Vector3(movement.x/100, -JUMP_POWER, movement.z/100))
+		#else:
+		#	h_velocity.x = 0
+		#	h_velocity.z = 0
 	
-	if is_on_wall(): #and direction != Vector3(0, 0, 0):
-		for i in range(get_slide_count()):
-			var collision = get_slide_collision(i)
-			if collision.normal.y != 1:
-				head_check.cast_to = Vector3(movement.x/2, 0, movement.z/2)
-				body_check.cast_to = Vector3(movement.x/2, 0, movement.z/2)
-				foot_check.cast_to = Vector3(movement.x/2, 0, movement.z/2)
-				print(str(head_check.is_colliding()) + "\n" + str(body_check.is_colliding()) + "\n" + str(foot_check.get_collision_point()))
-				if  (not head_check.is_colliding()) and (not body_check.is_colliding()) and (foot_check.is_colliding()):
-					label.set_text("Steppy!")
-					move_and_collide(Vector3(0, JUMP_POWER, 0))
-					move_and_collide(Vector3(movement.x/10, -JUMP_POWER, movement.z/10))
-	else:
-		label.set_text("")
+	if is_on_ceiling():
+		gravity_vec.y = -0.1
+		move_and_collide(Vector3(0, gravity_vec.y, 0))
 	
-	if direction != Vector3(0, 0, 0):
-		move_and_slide_with_snap(movement * MOVE_SPEED, snap, Vector3.UP)
+	if direction != Vector3(0, 0, 0) and (not is_on_ceiling()):
+		move_and_slide_with_snap(movement, snap, Vector3.UP)
 		snap = Vector3.DOWN
 	else:
-		move_and_slide(movement * MOVE_SPEED, Vector3.UP)
+		move_and_slide(movement, Vector3.UP)
 	
 	#Cursor Capture
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -104,6 +128,9 @@ func _physics_process(delta):
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			
+	if Input.is_action_just_pressed("interact_1"):
+		pass
 
 
 #Old stuffs
@@ -149,15 +176,15 @@ func _physics_process(delta):
 #	if is_on_floor():
 #		velocity.y = -0.01
 #		#Walking
-#		velocity.x = MOVE_SPEED * direction.x
-#		velocity.z = MOVE_SPEED * direction.z
+#		velocity.x = move_speed * direction.x
+#		velocity.z = move_speed * direction.z
 #		if Input.is_action_just_pressed("movement_jump"):
 #			velocity.y += JUMP_POWER
 #	elif not is_on_floor():
 #		velocity.y -= delta * GRAVITY
 #		#Air Strafing
-#		velocity.x = AIR_MOVE_SPEED * direction.x
-#		velocity.z = AIR_MOVE_SPEED * direction.z
+#		velocity.x = AIR_move_speed * direction.x
+#		velocity.z = AIR_move_speed * direction.z
 #
 #
 #
---
+++ objects/Object.gd
@@ -0,0 +1,7 @@
+extends Spatial
+
+#Cursor types: none, talk, collect, interact
+export var cursor = "none"
+
+func _ready():
+	pass
